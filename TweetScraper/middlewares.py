9654efa275b6a95f7cb019feb98e293704052b05
“valar000” <valar000@outlook.com>
2020-03-14 14:18:14 +0800

    fix retry

--- TweetScraper/middlewares.py
+++ TweetScraper/middlewares.py
@@ -18,10 +18,10 @@ class TooManyRequestsRetryMiddleware(RetryMiddleware):
             return response
         elif response.status == 429:
             self.crawler.engine.pause()
-            time.sleep(1) # If the rate limit is renewed in a minute, put 60 seconds, and so on.
+            time.sleep(10) # If the rate limit is renewed in a minute, put 60 seconds, and so on.
             self.crawler.engine.unpause()
             reason = response_status_message(response.status)
-            return self._retry(request, reason, spider) or response
+            return self._retry(request, reason, spider) #or response
         elif response.status in self.retry_http_codes:
             reason = response_status_message(response.status)
             return self._retry(request, reason, spider) or response
